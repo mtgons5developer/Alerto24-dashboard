7d6ddab40e6ff02dd63bf3884e38a1ce631b69c6
Enam <surelastmail@gmail.com>
2022-03-31 21:48:20 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+
+                <div class="card-body">
+                 
+            <h2 class="text-center">In Development</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
