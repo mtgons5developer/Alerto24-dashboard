ee33721c7daf1891ab51d1ecfd70317d76be5b97
softsuite tech <softsuite123@gmail.com>
2022-04-14 22:41:41 +0500

    trade history

--- resources/views/admin_layouts/base.blade.php
+++ resources/views/admin_layouts/base.blade.php
@@ -252,15 +252,26 @@
 
 								<!--end::Languages-->
 								<!--begin::User-->
-								<div class="topbar-item">
-									<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
-										<span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-										<span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Sean</span>
-										<span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
-											<span class="symbol-label font-size-h5 font-weight-bold">S</span>
-										</span>
-									</div>
-								</div>
+                                <div class="topbar-item">
+                                    <div
+                                        class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2"
+                                        id="kt_quick_user_toggle">
+                                        <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">{{ auth()->user()->name }} <br> <small>[{{ auth()->user()->email }}]</small></span>
+
+                                    </div>
+
+                                    <a class="menu-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+
+                                        <span class="menu-text">{{ __('Logout') }}</span>
+
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
 								<!--end::User-->
 							</div>
 							<!--end::Topbar-->
