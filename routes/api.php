9f64907ba747f8af1d22ddb10c27ec3d091f1b77
softsuite tech <softsuite123@gmail.com>
2022-04-15 23:06:54 +0500

    Login and Register API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//
+//});
+    Route::post('/login',[LoginController::class,'customLogin']);
+    Route::post('/register',[LoginController::class,'customRegister']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::group(['middleware' => 'auth:sanctum'], function(){
+
+    });
