573a2d86820a8ac099e53d7bf774fc6312db9665
PJ <techdevelopers08@gmail.com>
2022-04-20 14:58:26 +0100

    Api work on login,register,me

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\VerifyEmailController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
@@ -22,13 +23,19 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 //
 //});
 
-Route::post('/login',[LoginController::class,'customLogin']);
-Route::post('/register',[LoginController::class,'customRegister']);
-Route::post('/register',[LoginController::class,'customRegister']);
+// Route::post('/login',[LoginController::class,'customLogin']);
+// Route::post('/register',[LoginController::class,'customRegister']);
+Route::post('/login',[UserController::class,'Login']);
+Route::post('/register',[UserController::class,'Register']);
+Route::get('/provinceListing',[UserController::class,'provinceListing']);
+Route::get('/cityListing',[UserController::class,'cityListing']);
+Route::get('/barangayListing',[UserController::class,'barangayListing']);
+// Route::post('/register',[LoginController::class,'customRegister']);
 Route::get('email/verify/{id}',[VerifyEmailController::class,'verify'])->name('verification.verify'); // Make sure to keep this as your route name
 
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/me',[LoginController::class,'me']);
+    // Route::post('/me',[LoginController::class,'me']);
+    Route::post('/Me',[UserController::class,'Me']);
     Route::get('email/resend', [LoginController::class,'resend'])->name('verification.resend');
 });
