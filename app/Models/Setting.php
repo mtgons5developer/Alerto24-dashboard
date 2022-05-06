1e92ae2b3cbc93c0290a8b469321698941525ab1
Ernesto H. Almeda Jr <mtgons5.ea@gmail.com>
2022-05-07 03:10:52 +0800

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -10,7 +10,7 @@ class Setting extends Model
     use HasFactory;
     protected $fillable=[
         'qty',
-        'datetime',
+        'deltatime',
         'timeframe',
         'toggle',
         'Error',
